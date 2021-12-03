d4d81069d1229e5e71a58b38f892a3a093ba6879
matheswaaran <matheswaarans@gmail.com>
2021-12-03 15:43:45 +0530

    TailwindCSS

--- boards/admin.py
+++ boards/admin.py
@@ -2,8 +2,28 @@ from django.contrib import admin
 
 from .models import Board, Topic, Post
 
+
 # Register your models here.
 
-admin.site.register(Board)
-admin.site.register(Topic)
-admin.site.register(Post)
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'is_active', 'created_at', 'updated_at')
+    list_filter = ('is_active',)
+    search_fields = ('name', 'description')
+    ordering = ('-created_at',)
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'is_active', 'created_at', 'updated_at', 'board', 'starter', 'views')
+    list_filter = ('is_active', 'starter',)
+    search_fields = ('subject',)
+    ordering = ('-created_at',)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('message', 'is_active', 'topic', 'created_at', 'updated_at', 'created_by', 'updated_by')
+    list_filter = ('is_active', 'created_by', 'updated_by',)
+    search_fields = ('message',)
+    ordering = ('-created_at',)
